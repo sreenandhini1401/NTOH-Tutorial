a251c6275d5cdaba1a809c682f450d64a6ca04e9
sreenandhini1401 <70833277+sreenandhini1401@users.noreply.github.com>
2022-05-25 11:17:14 +0530

    Update plot_Tsunami1d.m

--- plot_Tsunami1d.m
+++ plot_Tsunami1d.m
@@ -1,10 +1,11 @@
 n=801;
+del_x=0.125;
 for i=1:801
     h(1,i)=round(50-45*tanh(((del_x*(i-1))-70)/8));
 end
 dt=0.001
 h=-h/100;
-del_x=0.125;
+
 c=readmatrix('eta_1d.txt');
 c=c';
 x= 0: 0.125:100;
@@ -17,17 +18,35 @@ x= 0: 0.125:100;
 %          drawnow;
 %          refreshdata(p);
 % end
-s=c(500,:);g=c(1000,:);o=c(1500,:);l=c(2000,:);
+s=c(500,:);g=c(1000,:);o=c(1500,:);l=c(2000,:);b=c(3000,:);
              figure(1);
              plot(x,s,x,h);
+             xlabel('Distance from offshore(m)');
+             ylabel('Height(m)');
+             legend('Height','Bottom profile');
              title({['Tsunami propagation in seconds'];['time(\itt) = ',num2str(0.5)]});
              figure(2);
              plot(x,g,x,h);
+             xlabel('Distance from offshore(m)');
+             ylabel('Height(m)');
+             legend('Height','Bottom profile');
              title({['Tsunami propagation in seconds'];['time(\itt) = ',num2str(1)]});
              figure(3);
              plot(x,o,x,h);
+             xlabel('Distance from offshore(m)');
+             ylabel('Height(m)');
+             legend('Height','Bottom profile');
              title({['Tsunami propagation in seconds'];['time(\itt) = ',num2str(1.5)]});
              figure(4);
              plot(x,l,x,h);
+             xlabel('Distance from offshore(m)');
+             ylabel('Height(m)');
+             legend('Height','Bottom profile');
              title({['Tsunami propagation in seconds'];['time(\itt) = ',num2str(2)]});
+             figure(5);
+             plot(x,b,x,h);
+             xlabel('Distance from offshore(m)');
+             ylabel('Height(m)');
+             legend('Height','Bottom profile');
+             title({['Tsunami propagation in seconds'];['time(\itt) = ',num2str(3)]});
        
